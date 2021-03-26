06a61077811875df064e482d3cbede61f882e12c
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-26 21:08:29 +0530

    Added code of Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,17 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 isPresent=1
+wagePerHr=20
+fullTimeHr=8
+salary=0
 randomNum=$((RANDOM%2))
 if [ $randomNum -eq $isPresent ]
 then
         echo "Employee Present"
+	salary=$(($wagePerHr * $fullTimeHr))
+	echo "Salary = $salary"
 else
         echo " Employee Absent"
+	echo "Salary = $salary"
 fi
 
 
