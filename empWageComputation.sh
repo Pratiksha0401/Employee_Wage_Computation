f4ddc240f02674d156c309795fcee91a8bc388ca
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-26 21:26:05 +0530

    Added : code of Part time employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,23 @@
+#!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-isPresent=1
+isFullTime=1
+isPartTime=2
 wagePerHr=20
 fullTimeHr=8
+partTimeHr=4
 salary=0
-randomNum=$((RANDOM%2))
-if [ $randomNum -eq $isPresent ]
+randomNum=$((RANDOM%3))
+if [ $randomNum -eq $isFullTime ]
 then
-        echo "Employee Present"
+   echo "Employee Present Full Time"
 	salary=$(($wagePerHr * $fullTimeHr))
 	echo "Salary = $salary"
+elif [ $randomNum -eq $isPartTime ]
+then
+	echo "Employee Present for Part time"
+   salary=$(($wagePerHr * $partTimeHr))
+   echo "Salary = $salary"
 else
-        echo " Employee Absent"
+   echo " Employee Absent"
 	echo "Salary = $salary"
 fi
-
-
