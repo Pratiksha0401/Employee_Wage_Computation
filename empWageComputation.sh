98a361e72fe0267032bc6bde8799d5caf54f28c6
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-26 21:50:04 +0530

    Added : code of Daily wagw using case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,21 +3,20 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 isFullTime=1
 isPartTime=2
 wagePerHr=20
-fullTimeHr=8
-partTimeHr=4
-salary=0
-randomNum=$((RANDOM%3))
-if [ $randomNum -eq $isFullTime ]
-then
-   echo "Employee Present Full Time"
-	salary=$(($wagePerHr * $fullTimeHr))
-	echo "Salary = $salary"
-elif [ $randomNum -eq $isPartTime ]
-then
-	echo "Employee Present for Part time"
-   salary=$(($wagePerHr * $partTimeHr))
-   echo "Salary = $salary"
-else
-   echo " Employee Absent"
-	echo "Salary = $salary"
-fi
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+    $isFullTime)
+        empHr=8
+        ;;
+    $isPartTime)
+        empHr=4
+        ;;
+    *)
+        empHr=0
+        ;;
+esac
+
+salary=$(($empHr*$wagePerHr))
+echo $salary
+
