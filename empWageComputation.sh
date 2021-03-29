c56b1dca05fe5a67db9f24ecf6db49dc703fbcb8
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-29 11:13:22 +0530

    Refactor : code to get work hours using function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,27 +6,31 @@ wagePerHr=20
 echo "Wage per Hour=20 Rs"
 daysInMonth=20
 workDays=0
-for (( i=1;i<=30;i++ ))
+function getWorkHours()
+{
+for (( i=1;i<=num;i++ ))
 do
 	empCheck=$((RANDOM%3))
 	case $empCheck in
 		$isFullTime)
 			empHr=$(($empHr+8))
-			((workDays++))
 			;;
     $isPartTime) 
 			empHr=$(($empHr+4))
-			((workDays++))
 			;;
     *)
         empHr=$(($empHr+0))
         ;;
 	esac
 done
-if [ $empHr -ge 100 ] || [ $workDays -ge 20 ]
+echo "$empHr"
+}
+read -p "Enter number of days in a month : " num
+workHours="$(getWorkHours $((num)))"
+if [ $(($workHours)) -ge 100 ]
 then
-	echo "Total working Hours=$empHr"
-	salary=$(($empHr*$wagePerHr))
+	echo "Total working Hours=$workHours"
+	salary=$(($workHours * $wagePerHr))
 	echo "Total Wage for a Month=$salary Rs."
 else
 	echo "Not completed working hours and working days"
