490bfbf6c5a40db3f45d7c7b94a4824e3c853b41
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-29 11:56:30 +0530

    Store the day with daily wage in array

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -x
+
+declare -A days_dailyWage
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 isFullTime=1
 isPartTime=2
@@ -6,31 +7,35 @@ wagePerHr=20
 echo "Wage per Hour=20 Rs"
 daysInMonth=20
 workDays=0
-function getWorkHours()
-{
+read -p "Enter total number of days in a month:" num
 for (( i=1;i<=num;i++ ))
 do
 	empCheck=$((RANDOM%3))
 	case $empCheck in
 		$isFullTime)
+			dailyWageFull=$((8*20))
+			days_dailyWage[$i]="$dailyWageFull"
 			empHr=$(($empHr+8))
+			((workDays++))
 			;;
-    $isPartTime) 
+    	$isPartTime)
+			dailyWagePart=$((4*20))
+			days_dailyWage[$i]="$dailyWagePart"
 			empHr=$(($empHr+4))
+			((workDays++))
 			;;
-    *)
-        empHr=$(($empHr+0))
-        ;;
+	   *)
+			days_dailyWage[$i]="0"
+        	empHr=$(($empHr+0))
+        	;;
 	esac
+echo "Day $i =" ${days_dailyWage[$i]}
 done
-echo "$empHr"
-}
-read -p "Enter number of days in a month : " num
-workHours="$(getWorkHours $((num)))"
-if [ $(($workHours)) -ge 100 ]
+echo "Total worked days=$workDays"
+echo "Total working Hours=$empHr"
+if [ $(($empHr)) -ge 100 ]
 then
-	echo "Total working Hours=$workHours"
-	salary=$(($workHours * $wagePerHr))
+	salary=$(($empHr * $wagePerHr))
 	echo "Total Wage for a Month=$salary Rs."
 else
 	echo "Not completed working hours and working days"
