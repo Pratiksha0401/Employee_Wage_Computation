64326a7debe574c6643b5f5376d01311a852ce92
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-29 09:29:06 +0530

    Calculate Wages till

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,21 +3,31 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 isFullTime=1
 isPartTime=2
 wagePerHr=20
+echo "Wage per Hour=20 Rs"
 daysInMonth=20
-empCheck=$((RANDOM%3))
-
-case $empCheck in
-    $isFullTime)
-        empHr=8
-        ;;
-    $isPartTime)
-        empHr=4
-        ;;
+workDays=0
+for (( i=1;i<=30;i++ ))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		$isFullTime)
+			empHr=$(($empHr+8))
+			((workDays++))
+			;;
+    $isPartTime) 
+			empHr=$(($empHr+4))
+			((workDays++))
+			;;
     *)
-        empHr=0
+        empHr=$(($empHr+0))
         ;;
-esac
-
-salary=$(($empHr*$wagePerHr*$daysInMonth))
-echo $salary
-
+	esac
+done
+if [ $empHr -ge 100 ] || [ $workDays -ge 20 ]
+then
+	echo "Total working Hours=$empHr"
+	salary=$(($empHr*$wagePerHr))
+	echo "Total Wage for a Month=$salary Rs."
+else
+	echo "Not completed working hours and working days"
+fi
